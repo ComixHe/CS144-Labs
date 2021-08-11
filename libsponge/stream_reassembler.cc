28737fd64e1e77311aaec52ec26ea36ced7e5f07
ComixHe <comixhe1895@outlook.com>
2021-08-12 00:28:07 +0800

    fix some issues, and do some works for lab5

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,83 +1,92 @@
 #include "stream_reassembler.hh"
+#include <iostream>
 
-StreamReassembler::StreamReassembler(const uint64_t capacity): _output(capacity),_capacity(capacity),
-                                                            _bytes_unassembled(0),_eof(false),_eof_pos(0) {}
+StreamReassembler::StreamReassembler(const uint64_t capacity): _output(capacity),_capacity(capacity){}
 
-void StreamReassembler::_insert_waiting(const seg& node){
-    if (_waiting.empty()) //重组区为空直接插入
+void StreamReassembler::_insert_waiting(const seg& new_seg){
+    if (_waiting.empty()) //为空直接插入
     {
-        _waiting.insert(node);
-        _bytes_unassembled += node.data.length();
+        _waiting.insert(new_seg);
+        _bytes_unassembled += new_seg.data.length();
         return;
     }
 
-    seg tmp = node;
-    auto it = _waiting.lower_bound(node); //定位node在重组区的位置
-    auto var_index = tmp.index, var_length = tmp.data.length();
-    if (it != _waiting.begin()) //若node不为首节点则判断是否能向左合并
+    seg tmp = new_seg;
+    auto it = _waiting.lower_bound(tmp); 
+    auto var_index = tmp.begin_index, var_length = tmp.data.length();
+    if (it != _waiting.begin()) //尝试向左合并
     {
-        --it;   
-        if (var_index < it->index + it->data.length() ) //相交或被包含
+        --it;
+        if (var_index <= it->begin_index + it->data.length() ) 
         {     
-            if (var_index + var_length <= it->index + it->data.length())      //被包含不处理
+            if (var_index + var_length <= it->begin_index + it->data.length())//被左边的报文段完全包含     
                 return;
-            tmp.data = it->data + tmp.data.substr(it->index + it->data.length() - var_index); //相交处理
-            tmp.index = it->index;
-            var_index = tmp.index; var_length = tmp.data.length();
+            tmp.data = it->data + tmp.data.substr(it->begin_index + it->data.length() - var_index);//删除重叠部分
+            tmp.begin_index = it->begin_index;
+            var_index = tmp.begin_index;
+            var_length = tmp.data.length();
             _bytes_unassembled -= it->data.length();
-            it = _waiting.erase(it); //合并后移除原节点
+            it = _waiting.erase(it);
         } 
         else
             ++it;
     }
-    while (it != _waiting.end() && var_index + var_length > it->index) //判断是否能向右合并，可能出现连续合并情况
+    while (it != _waiting.end() && var_index + var_length >= it->begin_index)//尝试向右连续合并
     {
-        if (var_index >= it->index && var_index + var_length < it->index + it->data.length()) //被包含则直接丢弃
+        if (var_index >= it->begin_index && var_index + var_length <= it->begin_index + it->data.length())  //被右边的报文完全包含
             return;
-        if (var_index + var_length < it->index + it->data.length()) //相交处理
-            tmp.data += it->data.substr(var_index + var_length - it->index);
+        if (it->begin_index <= var_index + var_length && it->begin_index + it->data.length() > var_index + var_length)
+            tmp.data += it->data.substr(var_index + var_length - it->begin_index);
         _bytes_unassembled -= it->data.length();   
-        it = _waiting.erase(it);    //合并完成移除节点
+        it = _waiting.erase(it);
     }
-    _waiting.insert(tmp);   //合并完成后插入重排区，也有可能没有合并操作发生
-    _bytes_unassembled += tmp.data.length(); 
+    _waiting.insert(tmp);      
+    _bytes_unassembled += tmp.data.length();
+    return;
 }
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const std::string &data, const uint64_t index, const bool eof) {
-    seg new_seg{data, index};
-    uint64_t first_unread = _output.bytes_read();
-    uint64_t first_unassembled = _output.bytes_written();
-    uint64_t first_unaccept = first_unread + _capacity;
-
-    if (index + data.length() < first_unassembled || index >= first_unaccept)  //超出范围直接丢弃
+    seg new_seg{data,index};
+    auto _first_unread = _output.bytes_read();
+    auto _first_unassembled = _first_unread + _output.buffer_size();
+    auto _first_unacceptable = _first_unread + _capacity;
+    if(data.empty())
+        goto END;
+    if(index >= _first_unacceptable || index + data.length() <= _first_unassembled) //完全超出边界故丢弃
         return;
-    if (index + data.length() > first_unaccept)
-        new_seg.data = new_seg.data.substr(0, first_unaccept - index); //部分超出做截断处理
-    if (index <= first_unassembled) //若新字串可以直接写入bytestream
+    if (index + data.length() > _first_unacceptable)//减去超出部分
+        new_seg.data = std::move(new_seg.data.substr(0, _first_unacceptable - index));
+    if (index <= _first_unassembled) 
     {   
-        _output.write(new_seg.data.substr(first_unassembled - index));
+        auto tmp_string = new_seg.data.substr(_first_unassembled - index);
+        _output.write(tmp_string); //减去多余部分后写入
+        _first_unassembled += tmp_string.length();
         auto it = _waiting.begin();
-        while (it->index <= _output.bytes_written() && it!=_waiting.end())  //检查重排区是否能继续写入
+        while (it->begin_index <= _first_unassembled && it!=_waiting.end())  //尝试把重组部分写入输出流
         {
-            if (it->index + it->data.length() > new_seg.index + new_seg.data.length()) 
-                _output.write(it->data.substr(_output.bytes_written() - it->index));
-            _bytes_unassembled -= it->data.length();
+            if(it->begin_index + it->data.length() >= _first_unassembled){
+                tmp_string = it->data.substr(_first_unassembled - it->begin_index);
+                _output.write(tmp_string);
+                _first_unassembled += tmp_string.length();
+                _bytes_unassembled -= it->data.length();
+            }
             it = _waiting.erase(it);
         }
     } 
     else
-        _insert_waiting(new_seg);    //不能直接写入则待重排
-
-    if (eof) 
-    {
-        _eof = true;
-        _eof_pos = index + data.length(); //确定流传输数量，以免还有数据在重排区却以为读取完毕
+        _insert_waiting(new_seg);//等待重组
+END:
+    if (eof){
+        _eof_flag = true;
+        _eof_pos = index + data.length();
     }
-    if (_eof && _output.bytes_written() == _eof_pos) //已经读取完毕，通知bytestream
-        _output.end_input();   
+    if (_eof_flag && _output.bytes_written() == _eof_pos)//收到关闭信号且最后一个字节已写入（可能有丢弃的所以没有全写入）
+        _output.end_input();
+    std::cout<<"bytes_unass:"<<_bytes_unassembled<<" bytes_written:"<<_output.bytes_written()<<" eof_pos:"<<_eof_pos<<std::endl;
+    return;
 }
 
 uint64_t StreamReassembler::unassembled_bytes() const { return _bytes_unassembled; }
